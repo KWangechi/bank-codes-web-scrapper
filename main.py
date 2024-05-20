dc2f8592e0bb65f54a52e64a2fdafcaff56ddedc
KWangechi <rachel.karanja@strathmore.edu>
2024-05-20 23:24:12 +0300

    ft: reading title from the extracted html file

--- main.py
+++ main.py
@@ -8,6 +8,20 @@ html_bytes = page_object.read();
 
 html = html_bytes.decode('utf-8');
 
-print(html);
+# print(html);
 
+# find the title index
+
+title = html.find("<title>");
+
+# print(title);
+
+# get the title text itself
+start_index = html.find("<title>") + len("<title>");
+
+last_index = html.find("</title>");
+
+title_text = html[start_index:last_index];
+
+print(title_text);
 
