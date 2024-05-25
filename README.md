1d24e116133c59d07071eac758a1c6e0275c3a0f
KWangechi <rachel.karanja@strathmore.edu>
2024-05-26 02:33:40 +0300

    ft: added a readme file to show project setup and contribution guidelines

---
+++ README.md
@@ -0,0 +1,19 @@
+## Bank Branch Codes
+- Small project for getting bank data together with their branch details
+- NB: This list is not exclusive and more data may need to be gathered and cross checked in order to provide accurate data
+- **Reason behind this**: Didn't find a Central Place for finding this data. One is forced to visit the website of every bank to find the info which I find abit tiring.
+- This is a hobby project and contributions are welcome
+
+### Cloning the Project
+- Install in local machine: `git clone https://github.com/KWangechi/bank-codes-web-scrapper.git`
+
+- **Setting Up Venv(Virtual Environment) - Windows**
+> You must have pip installed - python -m venv venv
+> Navigate to the Scripts folder then activate the virtual env > - cd venv/Scripts - .\activate
+
+That's it!! Your project is now setup
+
+### Contributing
+Contributions are welcome... Simply fork the repository into your Github Account and once done, submit a PR which will be reviewed and if all is good, your changes will be merged
+
+
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import re
 import json
+import pandas as pd
+
 
 url = "https://www.snowdesert.co.ke/resources/"
 page = urlopen(url)
@@ -30,40 +32,47 @@ for link in links:
         html = page.read().decode("utf-8")
         soup = BeautifulSoup(html, "html.parser")
         
+        
+        # read the table details(may need to use pandas)
+        tables = soup.find('table');
+        print(tables);
+            
+            
         bank_html_text = soup.getText();
         
 
-        # 1. Get the branch swift code and the bank code
-        match_swift_code = re.search(r"Swift Code:\s*([A-Za-z0-9]+)", bank_html_text, re.IGNORECASE)
-        match_bank_code = re.search(r"Bank Code:\s*([A-Za-z0-9]+)", bank_html_text, re.IGNORECASE)
+        # # 1. Get the branch swift code and the bank code
+        # match_swift_code = re.search(r"Swift Code:\s*([A-Za-z0-9]+)", bank_html_text, re.IGNORECASE)
+        # match_bank_code = re.search(r"Bank Code:\s*([A-Za-z0-9]+)", bank_html_text, re.IGNORECASE)
         
-        if (match_swift_code or match_bank_code):
-            bank_swift_code = match_swift_code.group(1);
-            bank_code = match_bank_code.group(1);
+        # if (match_swift_code or match_bank_code):
+        #     bank_swift_code = match_swift_code.group(1);
+        #     bank_code = match_bank_code.group(1);
             
-            bank_data_source.append((bank_name, bank_swift_code, bank_code));
+        #     bank_data_source.append((bank_name, bank_swift_code, bank_code));
             
-            print(bank_data_source);
+        #     print(bank_data_source);
             
-        else:
-            print("No Data Found");
+            
+        # else:
+        #     print("No Data Found");
 
 
 # print(len(bank_data_source));
 
-for new_bank_name, new_bank_swift_code, new_bank_code in bank_data_source:
-    # create a dictionary that will store this data and display it
-            bank_dict = {
-                'bank_name': new_bank_name,
-                'bank_swift_code': new_bank_swift_code,
-                'bank_code': new_bank_code
-            }
+# for new_bank_name, new_bank_swift_code, new_bank_code in bank_data_source:
+#     # create a dictionary that will store this data and display it
+#             bank_dict = {
+#                 'bank_name': new_bank_name,
+#                 'bank_swift_code': new_bank_swift_code,
+#                 'bank_code': new_bank_code
+#             }
             
-            banks_list.append(bank_dict)
-            print(banks_list)
-            # write this ditionary into a json
-            # banks_json
-            with open("banks.json", "w") as json_file:
-                json.dump(banks_list, json_file)   
+#             banks_list.append(bank_dict)
+#             print(banks_list)
+#             # write this ditionary into a json
+#             # banks_json
+#             with open("banks.json", "w") as json_file:
+#                 json.dump(banks_list, json_file)   
  
 
