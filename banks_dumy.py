a055f8786db899c0fd22c1064ca6eb766120a63c
KWangechi <rachel.karanja@strathmore.edu>
2024-06-06 17:50:05 +0300

    fix: remove trailing whitespace in the bank_code value update: nest branch info if the bank_name is the same, to avoid repetition - started working on that

--- banks_dumy.py
+++ banks_dumy.py
@@ -5,7 +5,9 @@ reader = PdfReader('Bank-and-Branches-July-2023v.pdf')
 pages = reader.pages
 
 header = ["Bank Name", "Bank Code", "Branch Code", "Branch Name"]
-branch_data = []
+
+bank_dict = {}
+bank_data = []
 
 for page in pages:
     text = page.extract_text()
@@ -17,28 +19,57 @@ for page in pages:
         branches = lines[1:]
 
     branches = lines[0:]
-    # print(branches)
+    branches_info = []
+
 
     for branch in branches:
         digit_index = next((i for i, c in enumerate(branch) if c.isdigit()), None)
         if digit_index is not None:
             bank_name = branch[:digit_index].strip()
-            bank_code = branch[digit_index:digit_index+3]
+            bank_code = branch[digit_index:digit_index+3].strip()
             branch_code = branch[digit_index+3:digit_index+6]
             branch_name = branch[digit_index+6:].strip()
+            
+            # print(bank_name)
+            if bank_name not in bank_dict and branch_code not in bank_dict:
+                bank_dict = {
+                    'bank_name': bank_name,
+                    'bank_code': bank_code,
+                    'branches': [
+                        {
+                            'branch_code': branch_code,
+                            'branch_name': branch_name
+                        }
+                    ]
+                }
+            bank_data.append(bank_dict)
         else:
             bank_name = branch
             bank_code = ""
             branch_code = ""
             branch_name = ""
+            
+        # only include what is unique
+        # bank_dict format
+        # bank_dict = {
+        #     'bank_name': 'Bank 1',
+        #     'bank_code': '001',
+        #     'branches': [
+        #         {
+        #             'branch_code': 'Branch 1',
+        #             'branch_name': 'Branch 1 Name'
+        #         }
+        #     ]
+        # }
+        # bank_dict = dict(zip(header, [bank_name, bank_code, branch_code, branch_name]))
+        # bank_data.append(bank_dict)
+        
+print(bank_data)
 
-        branch_dict = dict(zip(header, [bank_name, bank_code, branch_code, branch_name]))
-        branch_data.append(branch_dict)
-
-# Print the branch_data to verify
-# print(json.dumps(branch_data, indent=4))
+# Print the bank_data to verify
+# print(json.dumps(bank_data, indent=4))
 
 # Write the entire list to the JSON file
 with open("banks_info.json", "w") as f:
-    json.dump(branch_data, f, indent=4)
+    json.dump(bank_data, f, indent=4)
 
