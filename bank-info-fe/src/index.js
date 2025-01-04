e0e597359dedfefc45881af5959d0f9308083cf6
Kemboi-Dun <gahrmicc@gmail.com>
2025-01-05 00:25:46 +0300

    Update: Header alert -- added new

--- bank-info-fe/src/index.js
+++ bank-info-fe/src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import "react-loading-skeleton/dist/skeleton.css";
+import { NextUIProvider } from "@nextui-org/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
   </React.StrictMode>
 );
 
