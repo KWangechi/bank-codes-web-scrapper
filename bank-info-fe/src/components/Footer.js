d9c8731a8c3d1d4bd7d0f472d8f07a9b7f7d6565
Kemboi-Dun <gahrmicc@gmail.com>
2025-01-05 04:39:26 +0300

    Update: Added tooltip to the footer socials

--- bank-info-fe/src/components/Footer.js
+++ bank-info-fe/src/components/Footer.js
@@ -1,16 +1,42 @@
+import { Tooltip } from "@nextui-org/react";
+
 const Footer = () => {
   // get current year
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-gray-800 p-2 text-gray-300 w-full flex items-center justify-between gap-4">
+    <footer className="bg-gray-800 p-2 text-gray-300 w-full flex items-center justify-between gap-4 px-4">
       <div className="flex items-center space-x-4">
-        <a href="https://github.com/KWangechi" title="KWangechi">
-          <i className="fa-brands fa-github"></i>
-        </a>
-        <a href="https://twitter.com/Keshi72438439" title="KWangechi">
-          <i className="fa-brands fa-x-twitter"></i>
-        </a>
+        <Tooltip
+          content={
+            <div className="px-1 py-2">
+              <div className="text-small font-bold">
+                {" "}
+                <i className="fa-brands fa-github"></i> Github Account
+              </div>
+              <div className="text-tiny">KWangechi</div>
+            </div>
+          }
+        >
+          <a href="https://github.com/KWangechi" title="KWangechi">
+            <i className="fa-brands fa-github"></i>
+          </a>
+        </Tooltip>
+        <Tooltip
+          content={
+            <div className="px-1 py-2">
+              <div className="text-small font-bold">
+                {" "}
+                <i className="fa-brands fa-x-twitter"></i> Account
+              </div>
+              <div className="text-tiny">@Keshi72438439</div>
+            </div>
+          }
+        >
+          <a href="https://twitter.com/Keshi72438439" title="KWangechi">
+            <i className="fa-brands fa-x-twitter"></i>
+          </a>
+        </Tooltip>
       </div>
 
       <div className="flex  gap-1 items-center justify-center ">
